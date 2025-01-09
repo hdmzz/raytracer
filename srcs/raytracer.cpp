50b6b8d953143a4d3179711470ce30a1ea81fb06
hugo <hdamitzian@gmail.com>
2025-01-09 15:41:25 +0100

    genration image ppm

--- srcs/raytracer.cpp
+++ srcs/raytracer.cpp
@@ -10,21 +10,19 @@ int	main(void)
 	int	img_width = 256;
 	int	img_height = 256;
 
-	std::cout << "P3 width: " << img_width << " P3 height: " << img_height << std::endl;
+	std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
 	for (int j = 0; j < img_width; j++) {
 		for (int i = 0; i < img_height; i++) {
 			//i et j sont caster  en double, on se retrouve avec des chiffres entre 0 et -1
 			auto	r = double(i) / (img_width - 1);
 			auto	g = double(j) / (img_height - 1);
 			auto	b = 0.0;
-			if(i == 100 && j == 100)
-				std::cout << "r: " << r << " g: " << g << " b: " << b << std::endl;
 			//mutiplier par 255.999 pour s'assurer que les valeurs s'etalent bien sur la plage 0 ==> 255
 			int		ir = int(255.999 * r);
 			int		ig = int(255.999 * g);
 			int		ib = int(255.999 * b);
 
-			std::cout << "ir: " << ir << " ig: " << ig << " ib: " << ib << std::endl;
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		};
 	};
 	return (0);
